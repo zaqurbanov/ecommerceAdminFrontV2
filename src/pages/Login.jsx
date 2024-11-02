51f5327dbe80b93a6e166ed705cf3d3d9e365f06
zaurqurbanov <qurbanovzaur078@gmail.com>
2024-11-02 18:37:04 +0400

    updated last

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
     {errors.password && <p className='text-red-700 text-[10px]'>{errors.password}</p> }
     <input type="password" id="password"  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder='******' required onChange={handleChange} value={values.password} />
   </div>
-
+{/* Test */}
  
   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
 </form> 
